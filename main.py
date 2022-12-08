a73ffd75ce4062ab684c6782d1bbe3a14f4a6846
Yuuri-0708 <yuuri2313@gmail.com>
2022-12-08 15:57:15 +0900

    よしだの日記を新規追加

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.Yoshida_Diary import Yoshida_Diary
 
 # ↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
     DiarySample(), 
-    ]
+    Yoshida_Diary(), 
+]
 
 for d in diaries:
     print("---------------------------------")
